3dbfb2be4f1476ba12c143caaa1e9f777a3b8931
Oleh Davymuka <oleh.davymuka@whykingz.com>
2023-11-21 17:59:42 +0200

    Ref [#master]: fixes;

--- app/telegram/bot.py
+++ app/telegram/bot.py
@@ -1,13 +1,11 @@
 import logging
-import openai
 import os
-import requests
-import aiogram
-from dotenv import load_dotenv, find_dotenv
+
+import openai
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
-from aiogram.utils import executor
 from aiogram.dispatcher.filters import Command
+from aiogram.utils import executor
+from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 logging.basicConfig(level=logging.INFO)
@@ -19,22 +17,23 @@ if TOKEN is None:
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+
 # Example
 @dp.message_handler(Command("start"))
 async def cmd_start(message: types.Message) -> None:
     await message.answer("Привiт! Я ваш бот.")
 
-#Example
+
+# Example
 @dp.message_handler(Command("gpt_request"))
 async def handle_gpt_request(message: types.Message) -> None:
     response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt="Привi, як ти?",
-        max_tokens=100
+        engine="text-davinci-002", prompt="Привi, як ти?", max_tokens=100
     )
-    await message.answer(response['choices'][0]['text'])
+    await message.answer(response["choices"][0]["text"])
 
 
 if __name__ == "__main__":
     from aiogram import executor
+
     executor.start_polling(dp, skip_updates=True)
