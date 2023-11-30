2b9e30ffd5e68b29126e83dd1e38751d29980071
Dmitryi <vdv404901@gmail.com>
2023-11-30 17:24:59 +0200

    new test_bot.py

--- app/tests/test_bot.py
+++ app/tests/test_bot.py
@@ -1,54 +1,31 @@
+import sys
 import os
-import pytest
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
-from aiogram.dispatcher.middlewares import BaseMiddleware
-from aiogram.dispatcher import FSMContext
-from aiogram import executor
-from dotenv import load_dotenv, find_dotenv
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+)
+
+import unittest
+from unittest.mock import patch
+from aiogram import types
 from app.telegram.bot import dp, send_welcome
 
-load_dotenv(find_dotenv())
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
-
-
-@pytest.fixture
-def bot():
-    return Bot(BOT_TOKEN)
-
-
-def test_send_welcome(bot, monkeypatch):
-    monkeypatch.setattr(
-        executor, "start_polling", lambda *args, **kwargs: None
-    )
-
-    message = types.Message(
-        message_id=1,
-        chat=types.Chat(id=1, type="private"),
-        date=123456789,
-        text="/start",
-    )
-
-    executor.Dispatcher.set_current(dp)
-    executor.Dispatcher.set_current(dp)
-    executor.Dispatcher.set_current(dp)
-
-    async def fake_send_welcome(message: Message):
-        assert message.text == "/start"
-        await send_welcome(message)
-
-    with executor.Dispatcher.current().freeze():
-        dp.message_handlers.append(
-            types.MessageHandler(send_welcome, commands=["start"])
+class TestYourBot(unittest.TestCase):
+    @patch("aiogram.types.Message")
+    @patch("aiogram.types.Message.reply")
+    async def test_send_welcome(self, mock_reply, mock_message):
+        user_message = types.Message(
+            message_id=1,
+            chat=types.Chat(id=1, type="private"),
+            from_user=types.User(id=1, is_bot=False, first_name="Test"),
         )
-        dp.message_handlers.append(
-            types.MessageHandler(fake_send_welcome, commands=["start"])
-        )
-        executor.Dispatcher.set_current(dp)
-        dp.process_message(message)
+        await send_welcome(user_message)
+        mock_reply.assert_called_once_with("Привiт! Я ваш телеграм-бот.")
 
-    assert message.text == "/start"
-    executor.Dispatcher.set_current(dp)
+    def tearDown(self):
+        patch.stopall()
 
-    dp.message_handlers.clear()
+
+if __name__ == "__main__":
+    unittest.main()
