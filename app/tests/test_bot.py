aac970d80de71336907a88f7481352f42c10b3fe
Dmitryi <vdv404901@gmail.com>
2023-12-03 21:19:39 +0200

    edit test, edit workflow

--- app/tests/test_bot.py
+++ app/tests/test_bot.py
@@ -4,21 +4,26 @@ import os
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 )
-import pytest
+import unittest
+from unittest.mock import AsyncMock, MagicMock
 from aiogram import types
 from app.telegram.bot import dp, send_welcome
 
 
-@pytest.mark.asyncio
-async def test_send_welcome(mocker):
-    user_message = types.Message(
-        message_id=1,
-        chat=types.Chat(id=1, type="private"),
-        from_user=types.User(id=1, is_bot=False, first_name="Test"),
-    )
+class TestSendWelcome(unittest.IsolatedAsyncioTestCase):
+    async def test_send_welcome(self):
+        message = types.Message(
+            message_id=1,
+            chat=types.Chat(id=123),
+            from_user=types.User(id=456),
+            date=123456789,
+            text="/start",
+        )
+        mock_reply = AsyncMock()
+        message.reply = mock_reply
+        await send_welcome(message)
+        mock_reply.assert_called_once_with("Привiт! Я ваш телеграм-бот.")
 
-    mock_reply = mocker.patch("aiogram.types.Message.reply")
 
-    await send_welcome(user_message)
-
-    mock_reply.assert_called_once_with("Привiт! Я ваш телеграм-бот.")
+if __name__ == "__main__":
+    unittest.main()
