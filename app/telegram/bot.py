a7da8e0974921c321d0096c24429fcdffeae2093
Dmitryi <vdv404901@gmail.com>
2023-12-10 18:20:16 +0200

    test

--- app/telegram/bot.py
+++ app/telegram/bot.py
@@ -1,7 +1,6 @@
 import asyncio
-import logging
-import sys
 from os import getenv
+import logging
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
@@ -9,20 +8,19 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
 
-TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN")
 dp = Dispatcher()
 
 
-class Start:
-    @dp.message(CommandStart())
-    async def command_start_handler(self, message: Message) -> None:
-        """
-        This handler receives messages with `/start` command
-        """
-        if message.from_user:
-            await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
-        else:
-            await message.answer("Hello!")
+@dp.message(CommandStart())
+async def command_start_handler(self, message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    if message.from_user:
+        await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+    else:
+        await message.answer("Hello!")
 
 
 @dp.message()
@@ -39,13 +37,12 @@ async def echo_handler(message: types.Message) -> None:
 
 
 async def main() -> None:
-    if TOKEN:
-        bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    if BOT_TOKEN:
+        bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
         await dp.start_polling(bot)
     else:
         logging.error("Bot token is not provided.")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
--- app/tests/test_bot.py
+++ app/tests/test_bot.py
@@ -1,1 +1,46 @@
+import asyncio
+import pytest
+from aiogram import types
+from app.telegram.bot import main, dp
 
+
+@pytest.mark.asyncio
+async def test_command_start_handler():
+    message = types.Message(
+        chat=types.Chat(id=123, type="private"),
+        from_user=types.User(id=456, is_bot=False, username="test_user", full_name="Test User"),
+        date=123456789,
+        text="/start",
+    )
+
+    dp.update_process_timeouts = 0.1
+
+    with pytest.raises(asyncio.TimeoutError):
+        await asyncio.wait_for(
+            asyncio.gather(
+                main(),
+                dp.message_handlers.handlers[0][0](message),
+            ),
+            timeout=1,
+        )
+
+
+@pytest.mark.asyncio
+async def test_echo_handler():
+    message = types.Message(
+        chat=types.Chat(id=123, type="private"),
+        from_user=types.User(id=456, is_bot=False, username="test_user", full_name="Test User"),
+        date=123456789,
+        text="Hello, bot!",
+    )
+
+    dp.update_process_timeouts = 0.1
+
+    with pytest.raises(asyncio.TimeoutError):
+        await asyncio.wait_for(
+            asyncio.gather(
+                main(),
+                dp.message_handlers.handlers[None][0](message),
+            ),
+            timeout=1,
+        )
